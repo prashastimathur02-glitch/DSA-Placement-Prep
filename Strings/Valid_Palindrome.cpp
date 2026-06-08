902e81b637192d5c3c91bc368079596574ef3913
prashastimathur02-glitch <prashastimathur02@gmail.com>
2026-06-08 22:53:17 +0530

    Added LeetCode 125 - Valid Palindrome solution

---
+++ Strings/Valid_Palindrome.cpp
@@ -0,0 +1,25 @@
+// LeetCode 125
+// Valid Palindrome
+class Solution {
+public:
+    bool isPalindrome(string s){
+    int left=0;
+    int right=size(s)-1;
+    while(left<right){
+        while(left<right && !isalnum(s[left])){
+            left++;
+        }
+        while(left<right && !isalnum(s[right])){
+            right--;
+        }
+        if(tolower(s[left])!=tolower(s[right])){
+            return false;
+        } 
+        else{
+            left++;
+            right--;
+        }
+    }  
+    return true; 
+    }
+};
