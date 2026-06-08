3c367d1d770e51f7d7c793c52d93abe215fdffbe
prashastimathur02-glitch <prashastimathur02@gmail.com>
2026-06-08 23:13:42 +0530

    Added LeetCode 13 - Roman to Integer solution

---
+++ Hashing/Roman_to_Integer.cpp
@@ -0,0 +1,32 @@
+// LeetCode 13
+// Roman to Integer
+class Solution {
+public:
+int getValue(char c){
+    switch(c){
+        case 'I':return 1;
+        case 'V':return 5;
+        case 'X':return 10;
+        case 'L':return 50;
+        case 'C':return 100;
+        case 'D':return 500;
+        case 'M':return 1000;
+        default:return 0;
+    }
+}
+    int romanToInt(string s) {
+        int ans=0;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            int cval=getValue(s[i]);
+            if(i+1<n && cval<getValue(s[i+1])){
+                ans =ans-cval;
+            }
+            else{
+                ans =ans+cval;
+            }
+        }
+        return ans;
+    
+    }
+};
