8fa4f1806bc049c58dff2c11039c7eba89445112
prashastimathur02-glitch <prashastimathur02@gmail.com>
2026-06-09 23:27:54 +0530

    Added LeetCode 20 - Valid Parentheses solution

---
+++ Stack/Valid_Parentheses.cpp
@@ -0,0 +1,27 @@
+// LeetCode 20
+// Valid Parentheses
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('||s[i]=='['||s[i]=='{'){
+                st.push(s[i]);
+            }
+            else{
+                if((st.empty())){
+                    return false;
+                }
+                char top=st.top();
+                if((s[i]==')'&& top=='(')||(s[i]==']' && top=='[')||(s[i]=='}'&& top=='{')){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+
+    }
+};
