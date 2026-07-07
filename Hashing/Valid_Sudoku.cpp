42fee20497c38b51585195dff425acec5452eca4
Prashasti Mathur <prashastimathur02@gmail.com>
2026-07-07 23:54:33 +0530

    Added LeetCode 36 - Valid Sudoku solution

---
+++ Hashing/Valid_Sudoku.cpp
@@ -0,0 +1,26 @@
+// LeetCode 36
+// Valid Sudoku
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool rows[9][9] = {false};
+        bool cols[9][9] = {false};
+        bool boxes[9][9] = {false};
+        for (int r = 0; r < 9; ++r) {
+            for (int c = 0; c < 9; ++c) {
+                if (board[r][c] == '.'){
+                    continue;  
+                }     
+                int val = board[r][c] - '1';
+                int box_idx = (r / 3) * 3 + (c / 3);
+                if (rows[r][val] || cols[c][val] || boxes[box_idx][val]) {
+                    return false;
+                }
+                rows[r][val] = true;
+                cols[c][val] = true;
+                boxes[box_idx][val] = true;
+            }
+        }
+        return true;
+    }
+};
